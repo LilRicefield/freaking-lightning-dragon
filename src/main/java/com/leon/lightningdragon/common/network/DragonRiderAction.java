f87ff80057444c80576c535d2ab65a3b5ebfd70e
Leon <zackryan512@gmail.com>
2025-09-04 14:17:02 +0700

    Add files via upload

---
+++ src/main/java/com/leon/lightningdragon/common/network/DragonRiderAction.java
@@ -0,0 +1,18 @@
+package com.leon.lightningdragon.common.network;
+
+/**
+ * Enum for dragon rider actions to replace magic strings
+ */
+public enum DragonRiderAction {
+    NONE,           // No special action
+    TAKEOFF_REQUEST, // Request takeoff from ground
+    ACCELERATE,     // Start acceleration (L-Ctrl pressed)
+    STOP_ACCELERATE, // Stop acceleration (L-Ctrl released) 
+    ABILITY_USE,    // Use a named ability (start/one-shot)
+    ABILITY_STOP;   // Stop a named ability (for hold-to-use)
+    
+    public static DragonRiderAction fromOrdinal(int ordinal) {
+        DragonRiderAction[] values = values();
+        return ordinal >= 0 && ordinal < values.length ? values[ordinal] : NONE;
+    }
+}
