fca0997089f174d8c952a5d19a18dc49f9962376
Leon <zackryan512@gmail.com>
2025-09-04 16:44:31 +0700

    Add files via upload

---
+++ src/main/java/com/leon/lightningdragon/common/network/NetworkHandler.java
@@ -0,0 +1,33 @@
+// NetworkHandler.java
+package com.leon.lightningdragon.common.network;
+
+import com.leon.lightningdragon.LightningDragonMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+public class NetworkHandler {
+    private static final String PROTOCOL_VERSION = "1";
+
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
+            ResourceLocation.fromNamespaceAndPath(LightningDragonMod.MOD_ID, "main"),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
+
+    public static void register() {
+        int id = 0;
+        // TODO: Register new Dragon ability message when implemented
+        INSTANCE.registerMessage(id++, MessageDragonRideInput.class,
+                MessageDragonRideInput::encode,
+                MessageDragonRideInput::decode,
+                MessageDragonRideInput::handle);
+        // DragonKeyPacket removed; use MessageDragonRideInput + MessageDragonControl for unified input
+        INSTANCE.registerMessage(id++, MessageDragonControl.class,
+                MessageDragonControl::encode,
+                MessageDragonControl::decode,
+                MessageDragonControl::handle);
+        // Rider anchor sync disabled; use server-deterministic seat placement
+    }
+}
