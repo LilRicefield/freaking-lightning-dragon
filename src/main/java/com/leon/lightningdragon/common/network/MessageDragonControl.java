d9a921d439767c5ec4cfe7b49ea15c2838a0e0fa
Leon <zackryan512@gmail.com>
2025-09-06 13:12:36 +0700

    Add files via upload

---
+++ src/main/java/com/leon/lightningdragon/common/network/MessageDragonControl.java
@@ -0,0 +1,47 @@
+package com.leon.lightningdragon.common.network;
+
+import com.leon.lightningdragon.server.entity.dragons.LightningDragonEntity;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public record MessageDragonControl(int dragonId, byte controlState, double posX, double posY, double posZ) {
+
+    public static void encode(MessageDragonControl message, FriendlyByteBuf buf) {
+        buf.writeInt(message.dragonId());
+        buf.writeByte(message.controlState());
+        buf.writeDouble(message.posX());
+        buf.writeDouble(message.posY());
+        buf.writeDouble(message.posZ());
+    }
+
+    public static MessageDragonControl decode(FriendlyByteBuf buf) {
+        return new MessageDragonControl(
+                buf.readInt(),
+                buf.readByte(),
+                buf.readDouble(),
+                buf.readDouble(),
+                buf.readDouble()
+        );
+    }
+
+    public static void handle(MessageDragonControl message, Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> {
+            Player player = context.getSender();
+            if (player != null) {
+                Entity entity = player.level().getEntity(message.dragonId());
+                if (entity instanceof LightningDragonEntity dragon) {
+                    if (dragon.isOwnedBy(player)) {
+                        dragon.setControlState(message.controlState());
+                    }
+                }
+            }
+        });
+        context.setPacketHandled(true);
+    }
+}
+
