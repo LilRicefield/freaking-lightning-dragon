5be0679a92ef7d0b7c1ad525662e4633690d96b2
Leon <zackryan512@gmail.com>
2025-09-13 17:50:26 +0700

    Add files via upload

---
+++ src/main/java/com/leon/lightningdragon/client/ClientProxy.java
@@ -0,0 +1,7 @@
+package com.leon.lightningdragon.client;
+
+public class ClientProxy {
+    
+    public void clientInit() {
+    }
+}
---
+++ src/main/java/com/leon/lightningdragon/client/particle/ParticleClientRegistry.java
@@ -0,0 +1,17 @@
+package com.leon.lightningdragon.client.particle;
+
+import com.leon.lightningdragon.LightningDragonMod;
+import com.leon.lightningdragon.common.registry.ModParticles;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = LightningDragonMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ParticleClientRegistry {
+    @SubscribeEvent
+    public static void registerParticleFactories(RegisterParticleProvidersEvent event) {
+        event.registerSpriteSet(ModParticles.LIGHTNING_STORM.get(), LightningStormParticle.Factory::new);
+        event.registerSpriteSet(ModParticles.LIGHTNING_ARC.get(), LightningStormParticle.FactoryArc::new);
+    }
+}
